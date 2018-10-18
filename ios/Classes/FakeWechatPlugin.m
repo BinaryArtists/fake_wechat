a6d44781fa57d533869f0ad2f81a9adae2cfd65c
林恒龙 <linhenglong@xrjiot.com>
2018-10-18 17:10:14 +0800

    Initial commit

---
+++ ios/Classes/FakeWechatPlugin.m
@@ -0,0 +1,20 @@
+#import "FakeWechatPlugin.h"
+
+@implementation FakeWechatPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"plugins.flutter.io/fake_wechat"
+            binaryMessenger:[registrar messenger]];
+  FakeWechatPlugin* instance = [[FakeWechatPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
