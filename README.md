2c0f3a3afe60e4879ee6dcb0cbe74e021d9b72e3
林恒龙 <linhenglong@xrjiot.com>
2018-10-24 12:27:30 +0800

    微信分享 - Android

--- README.md
+++ README.md
@@ -2,9 +2,59 @@
 
 A new Flutter plugin.
 
+## android
+
+````
+# 微信混淆
+-keep class com.tencent.mm.opensdk.** {*;}
+-keep class com.tencent.wxop.** {*;}
+-keep class com.tencent.mm.sdk.** {*;}
+````
+
+## ios
+
+````
+Capabilities
+Background Modes: Background fetch & Remote notifications
+````
+
+````
+在Xcode中，选择你的工程设置项，选中“TARGETS”一栏，在“info”标签栏的“URL type“添加“URL scheme”为你所注册的应用程序id
+
+URL Types
+weixin: identifier=weixin schemes=appId
+````
+
+````
+iOS 9系统策略更新，限制了http协议的访问，此外应用需要在“Info.plist”中将要使用的URL Schemes列为白名单，才可正常检查其他应用是否安装。
+
+<key>LSApplicationQueriesSchemes</key>
+<array>
+    <string>weixin</string>
+</array>
+<key>NSAppTransportSecurity</key>
+<dict>
+    <key>NSAllowsArbitraryLoads</key>
+    <true/>
+</dict>
+````
+
+## flutter
+
+````
+# pubspec.yml
+
+dependencies:
+  fake_wechat:
+    git:
+      url: http://git.xrjiot.cn/flutter_packages/fake_wechat.git
+````
+
+[示例](./example/lib/main.dart)
+
 ## Getting Started
 
 For help getting started with Flutter, view our online
 [documentation](https://flutter.io/).
 
-For help on editing plugin code, view the [documentation](https://flutter.io/developing-packages/#edit-plugin-package).
+For help on editing plugin code, view the [documentation](https://flutter.io/platform-plugins/#edit-code).
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,56 +1,149 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
 import 'dart:async';
 
-import 'package:flutter/services.dart';
 import 'package:fake_wechat/fake_wechat.dart';
 
-void main() => runApp(new MyApp());
-
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => new _MyAppState();
+void main() {
+  runZoned(() {
+    runApp(new MyApp());
+  }, onError: (dynamic error, dynamic stack) {
+    print(error);
+    print(stack);
+  });
 }
 
-class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    FakeWechat wechat = new FakeWechat();
+    wechat.registerApp(appId: 'xxxx');// 更换为目标应用的appId
+    return new FakeWechatProvider(
+        wechat: wechat,
+        child: new MaterialApp(
+          home: new Home(
+            wechat: wechat,
+          ),
+        ));
+  }
+}
+
+class Home extends StatefulWidget {
+  final FakeWechat wechat;
+
+  Home({
+    @required this.wechat,
+    Key key,
+  }) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return new _HomeState();
+  }
+}
+
+class _HomeState extends State<Home> {
+  StreamSubscription<FakeWechatAuthResp> _auth;
+  StreamSubscription<FakeWechatShareMsgResp> _share;
 
   @override
   void initState() {
     super.initState();
-    initPlatformState();
+    _auth = widget.wechat.authResp().listen(_listenAuth);
+    _share = widget.wechat.shareMsgResp().listen(_listenShareMsg);
   }
 
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      platformVersion = await FakeWechat.platformVersion;
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
+  void _listenAuth(FakeWechatAuthResp resp) {
+    String content = 'auth: ${resp.errorCode} ${resp.errorMsg}';
+    _showTips('登录', content);
+  }
+
+  void _listenShareMsg(FakeWechatShareMsgResp resp) {
+    String content = 'auth: ${resp.errorCode} ${resp.errorMsg}';
+    _showTips('分享', content);
+  }
+
+  @override
+  void dispose() {
+    if (_auth != null) {
+      _auth.cancel();
     }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
+    if (_share != null) {
+      _share.cancel();
+    }
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: const Text('Plugin example app'),
-        ),
-        body: new Center(
-          child: new Text('Running on: $_platformVersion\n'),
-        ),
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Fake Wechat Demo'),
       ),
+      body: new ListView(
+        children: <Widget>[
+          new ListTile(
+            title: new Text('环境检查'),
+            onTap: () async {
+              String content =
+                  'wechat: ${await widget.wechat.isWechatInstalled()} - ${await widget.wechat.isWechatSupportApi()}';
+              _showTips('环境检查', content);
+            },
+          ),
+          new ListTile(
+            title: new Text('登录'),
+            onTap: () {
+              widget.wechat.auth(
+                scope: [FakeWechatScope.SNSAPI_USERINFO],
+                state: 'auth',
+              );
+            },
+          ),
+          new ListTile(
+            title: new Text('文字分享'),
+            onTap: () {
+              widget.wechat.shareText(
+                scene: FakeWechatScene.TIMELINE,
+                text: 'Share Text',
+              );
+            },
+          ),
+          new ListTile(
+            title: new Text('图片分享'),
+            onTap: () async {
+              AssetImage image = new AssetImage('images/icon/timg.jpeg');
+              AssetBundleImageKey key = await image.obtainKey(createLocalImageConfiguration(context));
+              ByteData imageData = await key.bundle.load(key.name);
+              widget.wechat.shareImage(
+                  scene: FakeWechatScene.TIMELINE,
+                  imageData: imageData.buffer.asUint8List()
+              );
+            },
+          ),
+          new ListTile(
+            title: new Text('网页分享'),
+            onTap: () {
+              widget.wechat.shareWebpage(
+                  scene: FakeWechatScene.TIMELINE,
+                  webpageUrl: 'https://www.baidu.com'
+              );
+            },
+          )
+        ],
+      ),
+    );
+  }
+
+  void _showTips(String title, String content) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return new AlertDialog(
+          title: new Text(title),
+          content: new Text(content),
+        );
+      },
     );
   }
 }
