ea1096d2eb6221295ffaab9363edef93c611d026
林恒龙 <linhenglong@xrjiot.com>
2018-10-24 12:48:45 +0800

    删除Test

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -13,13 +13,5 @@ void main() {
   testWidgets('Verify Platform version', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(new MyApp());
-
-    // Verify that platform version is retrieved.
-    expect(
-        find.byWidgetPredicate(
-          (Widget widget) =>
-              widget is Text && widget.data.startsWith('Running on:'),
-        ),
-        findsOneWidget);
   });
 }
